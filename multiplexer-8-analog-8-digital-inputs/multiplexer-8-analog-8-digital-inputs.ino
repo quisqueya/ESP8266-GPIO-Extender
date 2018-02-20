c32252244897652daddaee9bf9b3fc3e1aa51c63
quisqueya <36648836+quisqueya@users.noreply.github.com>
2018-02-19 19:55:38 -0500

    Add files via upload

---
+++ multiplexer-8-analog-8-digital-inputs/multiplexer-8-analog-8-digital-inputs.ino
@@ -0,0 +1,119 @@
+#include "multiplexer-CD74HC4067-16-lines.h"
+
+MultiAnalogDigitalRead multiread;
+
+void setup() {
+  // put your setup code here, to run once:
+     Serial.begin(115200);
+     while (!Serial) 
+     {
+         ; // wait for serial port to connect. Needed for native USB port only
+     }
+     Serial.flush();
+     Serial.println("");
+
+    multiread.enableSerialMonitor =true; 
+    multiread.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    
+    Serial.println("ready to read digital pin 0 in 3 seconds");
+    delay(3000);
+    int val=multiread.readDigital(0);
+    Serial.print("read digital pin 0 value=");
+    Serial.println(val);
+    //------------------------------------
+    Serial.println("ready to read digital pin 1 in 3 seconds");
+    delay(3000);
+    val=multiread.readDigital(1);
+    Serial.print("read digital pin 1 value=");
+    Serial.println(val);
+    //------------------------------------
+    Serial.println("ready to read digital pin 2 in 3 seconds");
+    delay(3000);
+    val=multiread.readDigital(2);
+    Serial.print("read digital pin 2 value=");
+    Serial.println(val);
+    //------------------------------------
+    Serial.println("ready to read digital pin 3 in 3 seconds");
+    delay(3000);
+    val=multiread.readDigital(3);
+    Serial.print("read digital pin 3 value=");
+    Serial.println(val);
+    //------------------------------------
+     Serial.println("ready to read digital pin 4 in 3 seconds");
+    delay(3000);
+    val=multiread.readDigital(4);
+    Serial.print("read digital pin 4 value=");
+    Serial.println(val);
+    //------------------------------------
+    Serial.println("ready to read digital pin 5 in 3 seconds");
+    delay(3000);
+    val=multiread.readDigital(5);
+    Serial.print("read digital pin 5 value=");
+    Serial.println(val);
+    //------------------------------------
+    Serial.println("ready to read digital pin 6 in 3 seconds");
+    delay(3000);
+    val=multiread.readDigital(6);
+    Serial.print("read digital pin 6 value=");
+    Serial.println(val);
+    //------------------------------------
+    Serial.println("ready to read digital pin 7 in 3 seconds");
+    delay(3000);
+    val=multiread.readDigital(7);
+    Serial.print("read digital pin 7 value=");
+    Serial.println(val);
+    //------------------------------------
+    //=================================================================
+    Serial.println("ready to read digital pin 0 in 3 seconds");
+    delay(3000);
+    val=multiread.readAnalog(0);
+    Serial.print("read analog pin 0 value=");
+    Serial.println(val);
+    //------------------------------------
+    Serial.println("ready to read digital pin 1 in 3 seconds");
+    delay(3000);
+    val=multiread.readAnalog(1);
+    Serial.print("read analog pin 1 value=");
+    Serial.println(val);
+    //------------------------------------
+    Serial.println("ready to read digital pin 2 in 3 seconds");
+    delay(3000);
+    val=multiread.readAnalog(2);
+    Serial.print("read analog pin 2 value=");
+    Serial.println(val);
+    //------------------------------------
+    Serial.println("ready to read digital pin 3 in 3 seconds");
+    delay(3000);
+    val=multiread.readAnalog(3);
+    Serial.print("read analog pin 3 value=");
+    Serial.println(val);
+    //------------------------------------
+     Serial.println("ready to read digital pin 4 in 3 seconds");
+    delay(3000);
+    val=multiread.readAnalog(4);
+    Serial.print("read analog pin 4 value=");
+    Serial.println(val);
+    //------------------------------------
+    Serial.println("ready to read digital pin 5 in 3 seconds");
+    delay(3000);
+    val=multiread.readAnalog(5);
+    Serial.print("read analog pin 5 value=");
+    Serial.println(val);
+    //------------------------------------
+    Serial.println("ready to read digital pin 6 in 3 seconds");
+    delay(3000);
+    val=multiread.readAnalog(6);
+    Serial.print("read analog pin 6 value=");
+    Serial.println(val);
+    //------------------------------------
+    Serial.println("ready to read digital pin 7 in 3 seconds");
+    delay(3000);
+    val=multiread.readAnalog(7);
+    Serial.print("read analog pin 7 value=");
+    Serial.println(val);
+    //------------------------------------
+}
---
+++ multiplexer-8-analog-8-digital-inputs/multiplexer-CD74HC4067-16-lines.cpp
@@ -0,0 +1,100 @@
+#include "multiplexer-CD74HC4067-16-lines.h"
+
+bool MultiAnalogDigitalRead::enableSerialMonitor=false;
+
+
+MultiAnalogDigitalRead::MultiAnalogDigitalRead()
+{
+     initialized = false;
+}
+MultiAnalogDigitalRead::~MultiAnalogDigitalRead()
+{
+  
+}
+
+
+void MultiAnalogDigitalRead::begin()
+{
+  
+   if(enableSerialMonitor)
+   {  
+        Serial.println("================================================");
+        Serial.println("**** staring multi analog/digital inputs Driver class ****");
+   }  
+   
+   // *** selectPin = D0 ==> LOW = analog read select; HIGH = digital read select
+   pinMode(selectPin,OUTPUT);
+ 
+   // digitalReadPin  = D8;  // digital data pin
+    pinMode(digitalReadPin,INPUT);
+
+    // addressPins0 = D1;
+    // addressPins1 = D2;
+    // addressPins2 = D4;  //Address pins A0, A1, A2
+    pinMode(addressPins0,OUTPUT);
+    pinMode(addressPins1,OUTPUT);
+    pinMode(addressPins2,OUTPUT);
+
+    initialized=true;
+}
+int MultiAnalogDigitalRead::readDigital(int pin)
+{
+     if(pin<0 || pin>7)
+     {
+           if(enableSerialMonitor)
+           {                             
+                Serial.print("Invalid pin number ->");
+                Serial.println(pin+8);
+           }    
+           return -1;        
+     }        
+     setAddress(pin+8);
+     int val=digitalRead(digitalReadPin);
+     return val;
+}
+int MultiAnalogDigitalRead::readAnalog(int pin)
+{  
+     if(pin<0 || pin>7)
+     {
+           if(enableSerialMonitor)
+           {                             
+                Serial.print("Invalid pin number ->");
+                Serial.println(pin);
+           }    
+           return -1;        
+     }           
+     setAddress(pin);  
+     int val=analogRead(analogReadPin);
+     return val;
+}
+void MultiAnalogDigitalRead::setAddress(int pinNumber)
+{
+    if(enableSerialMonitor)
+    {                             
+        Serial.print("CALLED setAddress(");
+        Serial.print(pinNumber);
+        Serial.println(")");
+    }    
+    if(!initialized)
+    {
+          if(enableSerialMonitor)
+          {                             
+              Serial.println("*** MultiAnalogDigitalRead Class instance not initialized. Call begin() first to initialize this class. ***");
+          }    
+          return;
+     }
+    
+    //set address line 0
+    int address= pinNumber & 0x01;
+    digitalWrite(addressPins0, address);
+   
+    address= (pinNumber & 0x02) >> 1;
+    digitalWrite(addressPins1, address);
+
+    address= (pinNumber & 0x04) >> 2;
+    digitalWrite(addressPins2, address);
+
+    address= (pinNumber & 0x08) >> 3;
+    digitalWrite(selectPin,address);
+}
+
